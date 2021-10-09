616bac91780e9872dec992e5b3abf62a925f1b32
javybush <3814@holbertonschool.com>
2021-10-09 11:53:54 -0700

    Hello, printf errors

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
-/*main - Entry point
- *Description: prints "with proper grammar, but the outcome is a piece of
- * * art,"
+/**main - Entry point
+ *Description: prints "with proper grammar, but the outcome is a piece of art"
  *Return: Always 0 (Success)
  */
 int main(void)
