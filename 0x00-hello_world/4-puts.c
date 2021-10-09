bcbf5855fbb1825bd80c1febf2fac2d311dec851
javybush <3814@holbertonschool.com>
2021-10-09 10:58:55 -0700

    description line 9 main

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,7 @@
  *Description: prints "Programming is like building a multilingual puzzle"
  *Return: Always 0 (Success)
  */
-int main(void)
+int main(void *main)
 {
 		puts("\"Programming is like building a multilingual puzzle");
 			return (0);
