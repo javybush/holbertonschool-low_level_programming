fc57e4f6b28fc9dc7d44b6a9afec9a00ab289f6e
javybush <3814@holbertonschool.com>
2021-10-09 12:18:01 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /*main: Entry point
- *Description: prints "with proper grammar, but the outcome is a piece of art"
- *Return: Always 0 (Success)
+ * *Description: prints "with proper grammar, but the outcome is a piece of art"
+ * *Return: Always 0 (Success)
  */
 int main(void)
 {
